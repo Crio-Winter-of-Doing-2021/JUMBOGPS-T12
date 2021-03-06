71c8768fd913b96ed70f88742398d201e0b9f18b
FlarrowVerse <archisman1999@gmail.com>
2021-03-06 10:36:10 +0530

    Added basic server code

---
+++ server/app/models/Assets.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose'); // mongoose
+
+/*
+Asset: {
+    id: a unique id,
+    name: Asset name,
+    type: asset type,
+    route: {
+        src: Source address,
+        dest: Destination address,
+        start: Time of departure from src
+        end: Estimated time of arrival(can change)
+    },
+    location: {
+        type: 'Feature'
+        geometery: {
+            type: 'Point',
+            coordinates: [[long, lat]]
+        },
+        properties: {
+            id,
+            name,
+            description
+        }
+    }
+}
+*/
+
+const AssetSchema = mongoose.Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    route: {
+        src: {
+            type: String,
+            required: true
+        },
+        dest: {
+            type: String,
+            required: true
+        },
+        start: {
+            type: Date,
+            required: true
+        },
+        end: {
+            type: Date,
+            required: true
+        }
+    },
+    location: {
+        type: {
+          type: String, 
+          enum: ['Point'], 
+          required: true
+        },
+        coordinates: {
+          type: [[Number]],
+          required: true
+        }
+    }
+});
+
+module.exports = mongoose.model('Asset', AssetSchema);
