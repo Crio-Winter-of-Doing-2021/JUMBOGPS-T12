eee61b83abced6e44211d93846d14357b79f9810
Tejeshwar Reddy <mbtejeshwarreddy@gmail.com>
2021-03-24 15:14:17 +0530

    Wrap App.js in Browser Router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {store} from "./store/store";
+import {BrowserRouter as Router} from 'react-router-dom'
 import { Provider } from "react-redux";
 
 ReactDOM.render(
   <Provider store={store}>
+      <Router>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Router>
   </Provider>,
   document.getElementById('root')
 );
