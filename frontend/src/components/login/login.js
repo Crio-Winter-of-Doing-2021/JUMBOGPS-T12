8584f73f345bd62e3249b11be761f0b33d6267e0
Tejeshwar Reddy <mbtejeshwarreddy@gmail.com>
2021-03-25 13:31:40 +0530

    store token in localstorage

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -2,6 +2,7 @@ import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { Button, Input, message } from "antd";
 import React from "react";
 import { withRouter } from "react-router-dom";
+import { loginUser } from '../../api/apli-client';
 
 
 class Login extends React.Component {
@@ -14,6 +15,27 @@ class Login extends React.Component {
           password: "",
         };
       }
+
+      handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const loginData = {
+            id: this.id,            
+            password: this.password,
+        };
+        console.log(loginData);
+
+        const login = await loginUser(loginData)
+        debugger;
+        if(login.status ===200){
+          console.log(login);
+          localStorage.setItem('token', login.data.token);
+          this.props.history.push('/map');
+        } else{
+          console.log("Login Error")
+        }
+       
+    };
 
       
 
@@ -31,28 +53,20 @@ class Login extends React.Component {
                   className="input-field"
                   prefix={<UserOutlined className="site-form-item-icon" />}
                   placeholder="Username"
-                  onChange={(e) => {
-                    this.setState({
-                      username: e.target.value,
-                    });
-                  }}
+                  onChange={event => this.id = event.target.value}
                 />
     
                 <Input.Password
                   className="input-field"
                   prefix={<LockOutlined className="site-form-item-icon" />}
                   placeholder="Password"
-                  onChange={(e) => {
-                    this.setState({
-                      password: e.target.value,
-                    });
-                  }}
+                  onChange={event => this.password = event.target.value}
                 />
     
                 <Button
                   loading={this.state.loading}
                   type="primary"
-                  onClick={this.login}
+                  onClick={this.handleSubmit}
                 >
                   Login
                 </Button>
