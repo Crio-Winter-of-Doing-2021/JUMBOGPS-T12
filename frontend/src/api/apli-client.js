442fc66618708fb56eeba371696de53fbbc262ff
Tejeshwar Reddy <mbtejeshwarreddy@gmail.com>
2021-03-25 13:31:04 +0530

    pass authentication token

--- frontend/src/api/apli-client.js
+++ frontend/src/api/apli-client.js
@@ -2,11 +2,30 @@ import axios from './axios';
 
 export const fetchAssets =  ()=>{
 
-    return axios.get('api/assets')
+
+    let token = localStorage.getItem('token') || "";
+    return axios.get('api/assets', {headers:{
+        token:token
+    }})
   
 }
 
 export const fetchAssetDetails = (id)=>{
-    const results  =  axios.get(`api/assets?id=${id}`)
+    let token = localStorage.getItem('token') || "";
+    const results  =  axios.get(`api/assets?id=${id}`, {headers:{
+        token: token
+    }})
     return  results;
 }
+
+export const fetchUserDetails = () => {
+    return axios.get('/users/');
+};
+
+export const loginUser = (user) => {
+    return axios.post('api/users/login', user);
+};
+
+export const registerUser = (user) => {
+    return axios.post('api/users/register', user);
+};
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
+import { config } from '../components/navigation/sidebar/config';
 
 const instance = axios.create({
     baseURL:'http://localhost:8081'
 })
-
 
 export default instance;
